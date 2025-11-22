12651a3c927b686e0a183dd038fa953a58d97d3c
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2025-11-22 10:02:55 -0800

    Add collision detection
    Changed colliders on vehicle. Wheels stick out further, may need to adjust but want to make sure the detection to interact with it is working right. Perhaps that's a separate component and will need to mask the vehicle collision for it

--- Pit Stop Panic/Assets/Scripts/Actors/Player.cs
+++ Pit Stop Panic/Assets/Scripts/Actors/Player.cs
@@ -12,6 +12,13 @@ namespace PSP.Actors
         [Header("Settings")]
         [SerializeField] private InputReader inputReader;
 
+        [Header("Collisions")]
+        [SerializeField] private float playerHeight = 1.8f;
+        [SerializeField] private float playerRadius = 0.1f;
+        [SerializeField] private float raycastHeight = 0.5f;
+        [SerializeField] private bool showRaycast = false;
+        [SerializeField] private Color raycastColor = Color.red;
+
         private Vector3 moveDirection;
 
         private void OnEnable() {
@@ -27,15 +34,47 @@ namespace PSP.Actors
         }
 
         private void Move() {
-            transform.position += moveDirection * moveSpeed * Time.deltaTime;
+            Vector3 p1 = transform.position + Vector3.up * raycastHeight;
+            Vector3 p2 = transform.position + Vector3.up * (playerHeight - raycastHeight);
+            float moveDistance = moveSpeed * Time.deltaTime;
+            bool canMove = !Physics.CapsuleCast(p1, p2, playerRadius, moveDirection, moveDistance);
+
+            if (!canMove) {
+                // Try X only
+                Vector3 moveDirX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+                canMove = !Physics.CapsuleCast(p1, p2, playerRadius, moveDirX, moveDistance);
+
+                if (canMove) {
+                    moveDirection = moveDirX;
+                } else {
+                    // Try Z only
+                    Vector3 moveDirZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                    canMove = !Physics.CapsuleCast(p1, p2, playerRadius, moveDirZ, moveDistance);
+
+                    if (canMove) {
+                        moveDirection = moveDirZ;
+                    }
+                }
+            }
+
+            if (canMove) {
+                transform.position += moveDirection * moveDistance;
+            }
+            
             transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotateSpeed * Time.deltaTime);
         }
 
         // ----- EVENT LISTENERS -----
         private void OnMove(Vector2 movement) {
-            Debug.Log(movement);
             Vector2 inputVector = movement.normalized;
             moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (showRaycast) {
+                Debug.DrawRay(transform.position + Vector3.up * raycastHeight, moveDirection, raycastColor);
+            }
         }
     }
 }
